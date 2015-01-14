9454ccdf4e11a41a0017f969c52fcd6fe8324a44
Usuario Local <usuario_local@localhost.localdomain>
2015-01-14 15:10:36 +0100

    implementacion inicial

--- ParallelFilterMare.cc
+++ ParallelFilterMare.cc
@@ -21,25 +21,28 @@
 
 #define NPIXELX 1000
 #define NPIXELY 1000
+#define GRAX 1
+#define GRAY 1
+#define ARRAYX (int)(ceil(NPIXELX/(double)GRAX))
+#define ARRAYY (int)(ceil(NPIXELY/(double)GRAY))
 #include <mare/mare.h>
-#include <stdio.h>
+#include <stdio.h>   
 
 using namespace std;
-    
 
-mare::task_ptr task_im[1000][1000] = { NULL };
+mare::task_ptr task_im[ARRAYX][ARRAYY] = { NULL };
 
 //Compilación con gcc -o filtro filter.c -fopenmp -lm
 int main(int argc, char **argv){
     
     int px=NPIXELX;
     int py=NPIXELY;
-    int i,j;
+    int i=0,j=0;
     double sum, promedio;
     
     double im[NPIXELX][NPIXELY];
     mare::group_ptr g = mare::create_group("Filtro");
-
+    
     // "Lectura/Inicialización" de la imagen 
     for(i=0; i < px; i++)
         for(j=0; j < py; j++)
@@ -60,27 +63,64 @@ int main(int argc, char **argv){
         for(j=1; j < (py-1); j++)
             im[i][j] += 0.25 * sqrt((im[i-1][j]+im[i][j-1]));
     */
-    task_im[1][1] = mare::create_task([&im]{ im[1][1] += 0.25 * sqrt((im[0][1]+im[1][0]));});
-    mare::launch(g,task_im[1][1]);
-    for(i=2; i < px-1; i++){ 
-      task_im[i][1] = mare::create_task([&im,i]{ im[i][1] += 0.25 * sqrt((im[i-1][1]+im[i][0]));});
-      mare::after(task_im[i-1][1],task_im[i][1]);
-      mare::launch(g,task_im[i][1]);
+    i=0;
+    j=0; 
+
+    task_im[0][0] = mare::create_task([&im,&i,&j]{ 
+	int shiftx = GRAX*i+1;
+	int shifty = GRAY*j+1;
+	int w,z;
+	for (w = 0; (w<GRAX)&&((w+shiftx)<NPIXELX-1);w++)
+	  for (z = 0;(z<GRAY)&&((z+shifty)<NPIXELY-1);z++)
+	    im[w+shiftx][z+shifty] += 0.25 * sqrt((im[w+shiftx-1][z+shifty]+im[w+shiftx][z+shifty-1]));	
+      });
+    mare::launch(g,task_im[0][0]);
+    
+    for(i=1; i < ARRAYX; i++){ 
+      task_im[i][j] = mare::create_task([&im,&i,&j]{ 
+	int shiftx = GRAX*i+1;
+	int shifty = GRAY*j+1;
+	int w,z;
+	for (w = 0; (w<GRAX)&&((w+shiftx)<NPIXELX-1);w++)
+	  for ( z = 0;(z<GRAY)&&((z+shifty)<NPIXELY-1);z++)
+	    im[w+shiftx][z+shifty] += 0.25 * sqrt((im[w+shiftx-1][z+shifty]+im[w+shiftx][z+shifty-1]));
+      });
+      mare::after(task_im[i-1][0],task_im[i][0]);
+      mare::launch(g,task_im[i][0]);
     }
     
-    for(j=2; j < py-1; j++){
-      task_im[1][j] = mare::create_task([&im,j]{ im[1][j] += 0.25 * sqrt((im[0][j]+im[1][j-1]));});
-      mare::after(task_im[1][j-1],task_im[1][j]);
-      mare::launch(g,task_im[1][j]);
-    }
+    i=0;
+    j=0;
 
-    for( i=2; i < px-1;i++)
-      for(j=2; j < py-1; j++){
-	task_im[i][j] = mare::create_task([&im,i,j]{  im[i][j] += 0.25 * sqrt((im[i-1][j]+im[i][j-1])); });
+    for(j=1; j < ARRAYY; j++){
+      task_im[i][j] = mare::create_task([&im,&i,&j]{ 
+	int shiftx = GRAX*i+1;
+	int shifty = GRAY*j+1;
+	int w,z;
+	for (w = 0; (w<GRAX)&&((w+shiftx)<NPIXELX-1);w++)
+	  for ( z = 0;(z<GRAY)&&((z+shifty)<NPIXELY-1);z++)
+	    im[w+shiftx][z+shifty] += 0.25 * sqrt((im[w+shiftx-1][z+shifty]+im[w+shiftx][z+shifty-1]));
+      });
+      mare::after(task_im[0][j-1],task_im[0][j]);
+      mare::launch(g,task_im[0][j]);
+    }
+    for( i=1; i < ARRAYX;i++){
+      for(j=1; j < ARRAYY; j++){
+	task_im[i][j] = mare::create_task([&im,&i,&j]{ 
+	    int shiftx = GRAX*i+1;
+	    int shifty = GRAY*j+1;
+	    int w,z;
+	    for (w = 0; (w<GRAX)&&((w+shiftx)<NPIXELX-1);w++)
+	      for ( z = 0;(z<GRAY)&&((z+shifty)<NPIXELY-1);z++)
+		im[w+shiftx][z+shifty] += 0.25 * sqrt((im[w+shiftx-1][z+shifty]+im[w+shiftx][z+shifty-1]));
+	  });
 	mare::after(task_im[i-1][j],task_im[i][j]);
 	mare::after(task_im[i][j-1],task_im[i][j]);
 	mare::launch(g,task_im[i][j]);
       }
+    }
+
+    
     mare::wait_for(g);
     // Promedio tras el filtro (test de salida)
     sum = 0.0;
@@ -93,3 +133,5 @@ int main(int argc, char **argv){
     
     return 0; 
 }
+
+
